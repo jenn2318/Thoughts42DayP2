56e36fb8c2a4cffe69719b6d52837deae3153417
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-31 18:48:44 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE thoughts (
 id int NOT NULL AUTO_INCREMENT,
 thoughts_name VARCHAR(255) NOT NULL,
 thought_date INTEGER(6) NOT NULL,
-thought_archive VARCHAR(255) NOT NULL,
+thought_archive DATE NOT NULL,
 thought_updated BOOLEAN DEFAULT false,
 PRIMARY KEY (id)
 );
