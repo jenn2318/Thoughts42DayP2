56e2816dec1bf785e0250c88caae12d9cf286230
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-05-31 18:53:22 -0400

    adjeusted routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
     });
 
-    app.get("/thoughttank", function (req, res) {
+    app.get("/archive", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/archive.html"));
 
     });
