a4677bef7cf72dd0b42ab2415cfc471b948cfb34
Sarah Shelden <sarah.shelden113@gmail.com>
2018-06-02 10:03:21 -0400

    Refactor models and config sequelize

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
-// Dependencies
-let Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-let sequelize = new Sequelize("sequelize_chirpy", "root", "", {
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
-
-
-
-// Exports the connection for other files to use
-module.exports = sequelize;
+// // Dependencies
+// let Sequelize = require("sequelize");
+//
+// // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+// let sequelize = new Sequelize("thoughts_db", "root", "MyNewPass", {
+//     host: "localhost",
+//     port: 3306,
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         idle: 10000
+//     }
+// });
+//
+//
+//
+// // Exports the connection for other files to use
+// module.exports = sequelize;
