8d5659fc2fecc136348f3a0e7c019ce88e443385
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-06-04 01:43:54 -0400

    updated an image for modal2

--- public/thought42dayp2.js
+++ public/thought42dayp2.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-    $('.parallax').parallax();
-});
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.parallax');
-    var instances = M.Parallax.init(elems, options);
-});
+// $(document).ready(function(){
+//     $('.parallax').parallax();
+// });
+//
+//
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.parallax');
+//     var instances = M.Parallax.init(elems, options);
+// });
 
 // Or with jQuery
 
