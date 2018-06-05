53924159f4108edb5b141b69e9d9f4fbfde40f73
Joseph Kim <joekimga@gmail.com>
2018-06-05 07:01:12 -0400

    submit button console.log but not on dom

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ require('./routes/api-routes')(app);
 
 
 
-db.sequelize.sync({force:true}).then(function(){
+db.sequelize.sync({force: true}).then(function(){
 
     app.listen(PORT, function() {
         console.log("App listening on PORT: " + PORT);
