a469b44a33bbd95780b6beeeaa536f28a814c621
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-06-05 10:05:53 -0400

    checked routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,4 +1,4 @@
-var db = require("../models");
+const db = require("../models");
 const controller = require('../controllers/thoughts42dayp2_controllers')
 module.exports = function(app) {
     // Find all users and return them to the user with res.json
